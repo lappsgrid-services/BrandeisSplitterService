96a9b071e310d0de6fdac2cf1a533b7c58b804ab
Keigh Rim <keigh.rim@gmail.com>
2018-12-19 13:46:26 -0500

    now reads token from existing token view (last)

--- src/main/java/edu/brandeis/lapps/sentencesplitter/Splitter.java
+++ src/main/java/edu/brandeis/lapps/sentencesplitter/Splitter.java
@@ -29,6 +29,7 @@ import org.lappsgrid.serialization.lif.Annotation;
 import org.lappsgrid.serialization.lif.Container;
 import org.lappsgrid.serialization.lif.View;
 
+import java.util.List;
 import java.util.Map;
 
 import edu.brandeis.nlp.tokenizer.Tokenizer;
@@ -52,80 +53,70 @@ public class Splitter implements ProcessingService
         this.metadata = null;
     }
 
-    @Override
     /**
      * getMetadata simply returns metadata populated in the constructor
      */
+    @Override
     public String getMetadata() {
         return metadata;
     }
 
     @Override
     public String execute(String input) {
-        // Step #1: Parse the input.
-        Data data = Serializer.parse(input, Data.class);
+        Data data;
+        try {
+            data = Serializer.parse(input, Data.class);
+        } catch (Exception e) {
+            data = new Data();
+            data.setDiscriminator(Uri.TEXT);
+            data.setPayload(input);
+        }
 
-        // Step #2: Check the discriminator
         final String discriminator = data.getDiscriminator();
-        if (discriminator.equals(Uri.ERROR)) {
-            // Return the input unchanged.
-            return input;
+        Container container;
+        switch (discriminator) {
+            case Uri.ERROR:
+                // Return the input unchanged.
+                return input;
+            case Uri.JSON_LD:
+            case Uri.LIF:
+                container = new Container((Map) data.getPayload());
+                break;
+            case Uri.TEXT:
+                container = new Container();
+                container.setText((String) data.getPayload());
+                container.setLanguage("en");
+                break;
+            default:
+                String errorMsg = String.format("Unsupported discriminator: %s", discriminator);
+                return new Data<>(Uri.ERROR, errorMsg).asPrettyJson();
         }
-
-        // Step #3: Extract the text.
-        Container container = null;
-        if (discriminator.equals(Uri.TEXT)) {
-            container = new Container();
-            container.setText(data.getPayload().toString());
-        }
-        else if (discriminator.equals(Uri.LAPPS)) {
-            container = new Container((Map) data.getPayload());
-        }
-        else {
-            // This is a format we don't accept.
-            String message = String.format("Unsupported discriminator type: %s", discriminator);
-            return new Data<String>(Uri.ERROR, message).asJson();
-        }
-
         // Step #4: Create a new View
         View view = container.newView();
 
         // Step #5: Tokenize the text and add annotations.
         String text = container.getText();
 
-		// example code on how to run the splitter
-				
-		Tokenizer tokenizer = new Tokenizer();
-        TokenizedText result;
-		ArrayList<Token> tokens;
+        // example code on how to run the splitter
 
-		// for now overwriting the text
-		text = "John Jr. is asleep. Me too.";
-		
-		// getting the tokens, words and offsets should be taken from the Data object
-		tokens = new ArrayList<>();
-		tokens.add(new Token("John", 0, 4));
-		tokens.add(new Token("Jr.", 5, 8));
-		tokens.add(new Token("is", 9, 11));
-		tokens.add(new Token("asleep", 13, 19));
-		tokens.add(new Token(".", 20, 21));
-		tokens.add(new Token("Me", 22, 24));
-		tokens.add(new Token("too", 25, 28));
-		tokens.add(new Token(".", 29, 30));
-		
-		// running the tokenizer in split mode
-		result = tokenizer.splitText(text, tokens);
-		
-		// this would print to standard output
-		//result.printSentences();
-		
-		// You get the results from the sentences instance variable on the 
-		// TokenizedText instance (the result variable)
+        Tokenizer tokenizer = new Tokenizer();
+        TokenizedText result;
+        ArrayList<Token> tokens = new ArrayList<>();
+        List<View> tokenViews = container.findViewsThatContain(Uri.TOKEN);
+        View tokenView = tokenViews.get(tokenViews.size() - 1);
+        for (Annotation token : tokenView.getAnnotations()) {
+            int s = Math.toIntExact(token.getStart());
+            int e = Math.toIntExact(token.getEnd());
+            String tokenText = text.substring(s, e);
+            tokens.add(new Token(tokenText, s, e));
+        }
+        // running the tokenizer in split mode
+        result = tokenizer.splitText(text, tokens);
+
         int id = -1;
-		for (Sentence s : result.sentences) {
-			//System.out.println(String.format("<%d %d>", s.begin, s.end));
-			Annotation a = view.newAnnotation("s" + (++id), Uri.SENTENCE, s.begin, s.end);
-		}
+        for (Sentence s : result.sentences) {
+            Annotation a = view.newAnnotation("s" + (++id), Uri.SENTENCE, s.begin, s.end);
+        }
 
         // Step #6: Update the view's metadata. Each view contains metadata about the
         // annotations it contains, in particular the name of the tool that produced the
